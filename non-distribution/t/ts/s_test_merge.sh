cf95d4d07913bf138b74035962e50fa1ea623c07
Daniel Xiong <daniel@daniels-mbp-64.mynetworksettings.com>
2026-01-28 18:58:12 -0500

    added more tests

--- non-distribution/t/ts/s_test_merge.sh
+++ non-distribution/t/ts/s_test_merge.sh
@@ -4,14 +4,13 @@
 T_FOLDER=${T_FOLDER:-t}
 R_FOLDER=${R_FOLDER:-}
 
-cd "$(dirname "$0")/..$R_FOLDER" || exit 1
 
 DIFF=${DIFF:-diff}
 DIFF_PERCENT=${DIFF_PERCENT:-0}
 
 cat /dev/null > d/global-index.txt
 
-files=("$T_FOLDER"/d/m{1..3}.txt)
+files=("$T_FOLDER"/d/n{1..3}.txt)
 
 for file in "${files[@]}"
 do
@@ -19,8 +18,7 @@ do
     mv d/temp-global-index.txt d/global-index.txt
 done
 
-
-if DIFF_PERCENT=$DIFF_PERCENT t/gi-diff.js <(sort d/global-index.txt) <(sort "$T_FOLDER"/d/m4.txt) >&2;
+if DIFF_PERCENT=$DIFF_PERCENT t/gi-diff.js <(sort d/global-index.txt) <(sort "$T_FOLDER"/d/n4.txt) >&2;
 then
     echo "$0 success: global indexes are identical"
     exit 0
