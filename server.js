18ae32e8cc0e5143518767344254060508749a85
Daniel Xiong <daniel@daniels-mbp-64.mynetworksettings.com>
2026-02-27 18:52:18 -0500

    a

---
+++ server.js
@@ -0,0 +1,9 @@
+const node = require('./distribution/local/node.js');
+
+node.start((err) => {
+  if (err) {
+    console.error('Error starting node:', err);
+    process.exit(1);
+  }
+  console.log(`Node is running on ${node.config.ip}:${node.port}`);
+});
